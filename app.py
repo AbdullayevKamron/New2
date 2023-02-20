9f1d9057f576dad1c9963011aae8bb0e046fbd17
AbdullayevKamron <124658627+AbdullayevKamron@users.noreply.github.com>
2023-02-20 09:34:04 +0500

    New2
    New2 Hi

---
+++ app.py
@@ -0,0 +1,80 @@
+# # Set Methods
+my_set = {1,2,3,4,5}
+your_set = {4,5,6,7,8,9,10}
+#
+# # .difference()
+# print(your_set.difference(my_set))
+# print(my_set)
+#
+# # .discard()
+# print(my_set)
+# print(my_set.discard(4))
+# print(my_set)
+#
+# # .difference_update()
+# print(my_set)
+# print(my_set.difference_update(your_set))
+# print(my_set)
+# print(your_set)
+# print(your_set.difference_update(my_set))
+# print(your_set)
+#
+# # .intersection()
+# print(my_set.intersection(your_set))
+# print(my_set)
+#
+# # .intersection()
+# print(my_set & your_set)
+# print(my_set)
+#
+# # isdisjoint()
+# print(my_set.isdisjoint(your_set))
+# print(my_set)
+#
+# # issubset()
+# print(my_set.issubset(your_set))
+# print(my_set)
+#
+# # issuperset()
+print(your_set.issuperset(your_set))
+print(your_set)
+#
+# # .union()
+# print(my_set.union(your_set))
+# print(my_set)
+#
+# # .union()
+# print(my_set | your_set)
+# print(my_set)
+#
+# # .union_update()
+# my_set.union_update(your_set)
+# print(my_set)
+#
+# # .symmetric_difference()
+# print(my_set.symmetric_difference(your_set))
+# # print(my_set)
+#
+# # .symmetric_difference_update()
+# print(my_set.symmetric_difference_update(your_set))
+# print(my_set)
+#
+# # .update()
+# print(my_set.update(your_set))
+# print(my_set)
+#
+# # .pop()
+# print(my_set.pop())
+# print(my_set)
+#
+# # remove()
+# print(my_set.remove(5))
+# print(my_set)
+
+# # clear()
+# print(my_set.clear())
+# print(my_set)
+#
+# # add()
+# print(my_set.add(10))
+# print(my_set)fe
